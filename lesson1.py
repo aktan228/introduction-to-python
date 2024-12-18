e4e35b12cf31e9cf5f2989efc20ba0086cd8a26b
aktan <aktam200e@gmail.com>
2024-12-18 12:46:07 +0600

    third commit

--- lesson1.py
+++ lesson1.py
@@ -1,8 +1,18 @@
-a = 2
-b = 3
-c = 4
+# a = 2
+# b = 3
+# c = 4
+#
+#
+# print('2 * 2 =', 2*2)
+# print('2 * 3 =', 2*3)
+# print('2 * 4 =', 2*4)
 
+name = input('insert name: ')
+age = input('How old are u?: ')
+film = input('Which your favorite film?: ')
+book = input('which your favorite book?: ')
 
-print('2 * 2 =', 2*2)
-print('2 * 3 =', 2*3)
-print('2 * 4 =', 2*4)
+print('your name is', name + ', '
+    'you are', age, 'years old, '
+    'your favorite book is', book, ', '
+    'your favorite film is', film + '.')
