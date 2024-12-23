d47ba65afeddeaf30d56d0060e4274fcd7ca3bc7
aktan <aktam200e@gmail.com>
2024-12-23 14:41:46 +0600

    Aktan

--- Lesson2.py
+++ Lesson2.py
@@ -9,12 +9,49 @@
 # print('2 * 3 =', 2*3)
 # print('2 * 4 =', 2*4)
 """
-dawdawdawdawdwa
-
-dwadaw123er3wte4r5yhtujku
+name =
 """
 
 #   print('hi')
-name = 'Sam'
-name = 'Tom'
-print(name)
+# name1 = 'Sam'
+# name = 'tom'
+# print(NAME)
+# Name = 'Aktan'
+# NAME = 'DOG'
+# print(name)
+# print(Name)
+
+
+# num1 = 10
+# num2 = 12
+# summa = num1 + num2
+# minus = num1 - num2
+# multi = num1 * num2
+# div = num1 / num2
+# sqr = num1 ** num2
+# print(summa,minus,multi,div,sqr)
+
+
+# firstName = input('Insert your name: ')
+# lastName = input('Insert your name:')
+# fullName = firstName, lastName
+# print("Your fullname:", fullName)
+
+# pi = 3.14
+# r = 11
+#
+# a = 2 * pi * r ** 2
+# print(a)
+
+# c = 6
+#
+# far = c * 1.8 + 32
+# print(far)
+
+
+num1 = int(input("1 цифра: "))
+num2 = int(input("2 цифра: "))
+num3 = int(input("3 цифра: "))
+
+multiply = num1 * num2 * num3
+print(multiply)
